ad970164a6f535bc9d707562ee1cd2675cd1d891
greenbeanbrown <ahotebrown@gmail.com>
2019-08-16 23:11:43 -0400

    Added functionality to insert into MySql database
    Created DB connection using RMySQL.
    Added dynamic INSERT query that reacts upon user input.

--- app.R
+++ app.R
@@ -1,4 +1,14 @@
+#library(RMySQL)
+
+# create MySql connection - this is being hosted locally for now (hence the user/password)
+con <- dbConnect(MySQL(),
+                 user = 'remote',
+                 password = 'password',
+                 host = 'localhost',
+                 dbname = 'skate')
+
 ui <- fluidPage(
+  
   # make button
   actionButton(inputId = 'make',
                label = 'Make',
@@ -17,23 +27,58 @@ ui <- fluidPage(
 
 server <- function(input, output){
   
+  
   ##############################################################
-  # NEXT STEPS: 
   #
-  # CREATE A LOGGING SYSTEM FOR TRACKING DATA
-  #  -CREATE A DATABASE(POSTGRESQL OR MYSQL)
-  #  - CONNECT R SERVER SCRIPT TO THE DB 
-  #  - CREATE A REACTIVE FUNCTION THAT DYNAMICALLY UPDATES THE DB 
+  # COMPLETED:
+  #   - CREATED A LOGGING SYSTEM FOR TRACKING DATA
+  #     -CREATED A DATABASE (MYSQL)
+  #     - CONNECTED SERVER.R SCRIPT TO THE DB 
+  #     - CREATED REACTIVE FUNCTIONS THAT DYNAMICALLY UPDATE THE DB 
+  #
+  ##############################################################  
+  #
+  #  NEXT STEPS:
+  #   - ADD A selectInput() BOX TO ALLOW USER TO CHOOSE DIFFERENT TRICKS AND UPDATE DB ACCORDINGLY  
+  #   - NEED TO RESEARCH IF DISCONNECTING FROM THE DB IS NECESSARY AND IMPLEMENT IF NEED BE
+  #   - HOST THE APP ON A WEB SERVER
+  #
+  ##############################################################  
+  #
+  # MySql DB DETAILS: 
+  #       - NAME: skate
+  #       - TABLES: skate
+  #       - COLUMNS:
+  #           - attempt_id (auto-increment)
+  #           - date
+  #           - time
+  #           - trick
+  #           - attempt_nbr
+  #           - make
+  #           - miss
   #
   ##############################################################  
   
   # track the number of makes, misses, total attempts
   output$make <- renderText( { input$make })
   output$miss <- renderText( { input$miss })
-  output$total <- renderText({input$make + input$miss})
+  output$total <- renderText( { input$make + input$miss })
   
+  # INSERTING DATA INTO THE DATABASE
   
+  # make 
+  observeEvent(input$make, {
+    # upon clicking the make button, send an insert query with the made attempt
+    sql = paste0('INSERT INTO skate.skate ( date, time, trick, attempt_nbr, make, miss) VALUES (curdate(), curtime(), \'kickflip\', ',(input$make + input$miss),', ',input$make,', ',input$miss,')')
+    query <- dbSendQuery(con, sql)
+  })
   
+  # miss
+  observeEvent(input$miss, {
+    # upon clicking the make button, send an insert query with the missed attempt
+    sql = paste0('INSERT INTO skate.skate ( date, time, trick, attempt_nbr, make, miss) VALUES (curdate(), curtime(), \'kickflip\', ',(input$make + input$miss),', ',input$make,', ',input$miss,')')
+    query <- dbSendQuery(con, sql)
+  })
 }
 
 # run the app
